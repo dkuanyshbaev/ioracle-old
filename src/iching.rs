428d77aef486e67d700d2764594da4b94a652ba6
Denis K <orca84@protonmail.com>
2020-08-19 15:52:31 +1000

    WIP: operator

--- src/iching.rs
+++ src/iching.rs
@@ -3,42 +3,47 @@ use std::fmt;
 
 #[derive(Debug, Serialize)]
 pub enum Line {
-    Yang, // solid line
     Yin,  // open line
+    Yang, // solid line
 }
 
 impl fmt::Display for Line {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Line::Yang => write!(f, "Yang, solid line"),
             Line::Yin => write!(f, "Yin, open line"),
+            Line::Yang => write!(f, "Yang, solid line"),
+        }
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct Trigram {
+    pub top: Line,
+    pub middle: Line,
+    pub bottom: Line,
+}
+
+impl Trigram {
+    pub fn new() -> Trigram {
+        Trigram {
+            top: Line::Yin,
+            middle: Line::Yin,
+            bottom: Line::Yin,
         }
     }
 }
 
 #[derive(Serialize, Debug)]
 pub struct Hexagram {
-    pub first: Line,
-    pub second: Line,
-    pub third: Line,
-    pub fourth: Line,
-    pub fifth: Line,
-    pub sixth: Line,
-    pub seventh: Line,
-    pub eighth: Line,
+    pub top: Trigram,
+    pub bottom: Trigram,
 }
 
 impl Hexagram {
     pub fn new() -> Hexagram {
         Hexagram {
-            first: Line::Yang,
-            second: Line::Yang,
-            third: Line::Yang,
-            fourth: Line::Yang,
-            fifth: Line::Yang,
-            sixth: Line::Yang,
-            seventh: Line::Yang,
-            eighth: Line::Yang,
+            top: Trigram::new(),
+            bottom: Trigram::new(),
         }
     }
 }
