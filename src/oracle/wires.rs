635ab139bcbf14d44ed9879700cad8dcaa6c667c
Denis K <orca84@protonmail.com>
2020-08-24 23:19:44 +1000

    ADD: test buttons for settings

--- src/oracle/wires.rs
+++ src/oracle/wires.rs
@@ -1,59 +1,71 @@
 use crate::oracle::iching::Line;
+use rppal::gpio::Gpio;
+// use std::error::Error;
+// use std::thread;
+// use std::time::Duration;
 
 pub fn light(_line: &Line, line_num: u8) {
     println!("light line number {}", line_num);
 }
 
+pub fn on_off(pin: u8) {
+    if let Ok(gpio) = Gpio::new() {
+        if let Ok(pin) = gpio.get(pin) {
+            let mut pin = pin.into_output();
+            pin.set_high();
+            pin.set_low();
+        }
+    }
+}
+
 pub fn heaven(_colour: String, pin: u8) {
-    println!("heaven............");
-    println!("on {}", pin);
+    println!("----> heaven");
+
+    on_off(pin);
 }
 
 pub fn cloud(_colour: String, pin: u8) {
-    println!("cloud............");
-    println!("on {}", pin);
+    println!("----> cloud");
+
+    on_off(pin);
 }
 
 pub fn sun(_colour: String, pin: u8) {
-    println!("sun............");
-    println!("on {}", pin);
+    println!("----> sun");
+
+    on_off(pin);
 }
 
 pub fn wind(_colour: String, pin: u8) {
-    println!("ufffffffffffff............");
-    println!("on {}", pin);
+    println!("----> wind");
+
+    on_off(pin);
 }
 
 pub fn thunder(_colour: String, sound: String) {
-    println!("thunder............");
+    println!("----> thunder");
+
     println!("play {}", sound);
 }
 
 pub fn water(_colour: String, pin: u8) {
-    println!("water............");
-    println!("on {}", pin);
+    println!("----> water");
+
+    on_off(pin);
 }
 
 pub fn mountain(_colour: String, sound: String) {
-    println!("mountain............");
+    println!("----> mountain");
+
     println!("play {}", sound);
 }
 
-pub fn earth(_colour: String, pin: u8) {
-    println!("earth............");
-    println!("on {}", pin);
+pub fn earth(_colour: String, _pin: u8) {
+    println!("----> earth");
+
+    // on_off(pin);
 }
 
-// use std::error::Error;
-// use std::thread;
-// use std::time::Duration;
-//
-// use rppal::gpio::Gpio;
-// use rppal::system::DeviceInfo;
-//
-// // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-// const GPIO_LED: u8 = 23;
-//
 // fn main() -> Result<(), Box<dyn Error>> {
 //     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 //     let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
---
+++ static/js/test.js
@@ -0,0 +1,191 @@
+function heaven_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#heaven_pin').val();
+    var colour = $('#heaven_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/heaven",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function cloud_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#cloud_pin').val();
+    var colour = $('#cloud_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/cloud",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function sun_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#sun_pin').val();
+    var colour = $('#sun_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/sun",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function wind_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#wind_pin').val();
+    var colour = $('#wind_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/wind",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function thunder_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var sound = $('#thunder_sound').val();
+    var colour = $('#thunder_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : 0,
+        "colour" : colour,
+        "sound" : sound,
+    });
+
+    $.ajax({
+        url: "test/thunder",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function water_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#water_pin').val();
+    var colour = $('#water_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/water",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function mountain_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var sound = $('#mountain_sound').val();
+    var colour = $('#mountain_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : 0,
+        "colour" : colour,
+        "sound" : sound,
+    });
+
+    $.ajax({
+        url: "test/mountain",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
+
+function earth_test(){
+    $.ajaxSetup({
+        async: false
+    });
+
+    var pin = $('#earth_pin').val();
+    var colour = $('#earth_colour').val();
+
+    var data = JSON.stringify({
+        "pin" : parseInt(pin),
+        "colour" : colour,
+        "sound" : "",
+    });
+
+    $.ajax({
+        url: "test/earth",
+        type: "POST",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(){}
+    });
+};
