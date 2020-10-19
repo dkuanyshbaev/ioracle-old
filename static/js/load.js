d6c033539986f5170ff540e79c4c0fef4ef94937
Denis K <orca84@protonmail.com>
2020-10-20 01:38:56 +1000

    FIX

--- static/js/load.js
+++ static/js/load.js
@@ -1,7 +1,9 @@
 function commit(){
+    $('#main_container').hide();
+    $('#main_hr').hide();
     $('.header_to_hide').hide();
-    // $('#head1').hide();
-    // $('#head2').hide();
+    $('#head1').hide();
+    $('#head2').hide();
     $('.lead').hide();
     $('#commit_form').hide();
     $('#footer_block').hide();
