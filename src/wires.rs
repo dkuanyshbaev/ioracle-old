3edbff1c54b7cc66935ba0bb5efc99b0b03a93fd
Denis K <orca84@protonmail.com>
2020-10-23 18:28:24 +1000

    FIX

--- src/wires.rs
+++ src/wires.rs
@@ -563,7 +563,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 1: {}", line1);
-    line1.render(1, &mut controller, &settings.default_colour);
+    line1.render(6, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(3));
 
     let line2 = Line::read(
@@ -573,7 +573,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 2: {}", line2);
-    line2.render(2, &mut controller, &settings.default_colour);
+    line2.render(1, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(3));
 
     let line3 = Line::read(
@@ -583,7 +583,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 3: {}", line3);
-    line3.render(3, &mut controller, &settings.default_colour);
+    line3.render(2, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(1));
 
     let first_trigram = Trigram {
@@ -657,7 +657,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 4: {}", line4);
-    line4.render(4, &mut controller, &settings.default_colour);
+    line4.render(3, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(3));
 
     let line5 = Line::read(
@@ -667,7 +667,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 5: {}", line5);
-    line5.render(5, &mut controller, &settings.default_colour);
+    line5.render(4, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(3));
 
     let line6 = Line::read(
@@ -677,7 +677,7 @@ pub fn reading(settings: &Binding) -> IOracleResult<(Hexagram, Hexagram)> {
         settings.threshold.clone(),
     );
     println!("Line 6: {}", line6);
-    line6.render(6, &mut controller, &settings.default_colour);
+    line6.render(5, &mut controller, &settings.default_colour);
     thread::sleep(Duration::from_secs(1));
 
     let second_trigram = Trigram {
